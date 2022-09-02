8a36df6d16ce2a2f59d5b2c1c0c3b652fde04a59
w2009 <w2009@e-mirim.hs.kr>
2022-09-02 14:05:23 +0900

    admin NoticeAdmin

--- notice/admin.py
+++ notice/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from notice.models import Notice
+
+
+@admin.register(Notice)
+class NoticeAdmin(admin.ModelAdmin):
+    list_display = ('title','content','create_at','modify_at')
+    list_filter = ('modify_at',)
+    search_fields = ('title','content')
