0aee3c825dc12d08c060ffc26e8808ab70a7e6cf
w2009 <w2009@e-mirim.hs.kr>
2022-09-16 11:18:17 +0900

    views, templetes, urlsd notice:detail

--- notice/url.py
+++ notice/url.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from notice.views import NoticeListView
+from notice.views import NoticeListView, NoticeDetailView
 
 app_name = 'notice'
 
 urlpatterns = [
     path('', NoticeListView.as_view(), name='list'),
+    path('detail/<int:pk>/',NoticeDetailView.as_view(), name='detail'),
 ]
--- notice/views.py
+++ notice/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from notice.models import Notice
 
 
 class NoticeListView(ListView):
     model = Notice
+
+
+class NoticeDetailView(DetailView):
+    model = Notice #DB에서 pk에 해당하는 notice 가져와서
+                    #notice 라는 변수로 notice_detail.html파일로 전달
